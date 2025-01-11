31b9b4902bdcb650acd9b253a3b3d7081b1d880a
Souekbondol-ai <e20221592@dtc1.itc.edu.kh>
2025-01-11 22:06:25 +0700

    update admin feature

--- book_inventory/book/views.py
+++ book_inventory/book/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
 from django.db.models import Q
 from .models import Book, Category
 from django.core.paginator import Paginator
+from .forms import BookForm
+from django.contrib.auth.decorators import login_required
 
 class HomeView(ListView):
     model = Book
@@ -16,8 +18,6 @@ class HomeView(ListView):
         context['best_sellers'] = Book.objects.filter(is_best_seller=True)[:6]
         context['now_trending'] = Book.objects.filter(is_now_trending=True)[:6]
         context['award_winners'] = Book.objects.filter(is_award_winner=True)[:6]
-        for category in context['categories']:
-            context[f'{category.name.lower().replace(" ", "_")}_books'] = Book.objects.filter(category=category)[:6]
         return context
 
 class BookDetailView(DetailView):
@@ -26,24 +26,27 @@ class BookDetailView(DetailView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Get current book
         book = self.get_object()
-        context['related_books'] = Book.objects.filter(
-            category=book.category
-        ).exclude(id=book.id)[:4]
-        
+        context['related_books'] = Book.objects.filter(category=book.category).exclude(id=book.id)[:4]
+        context['categories'] = Category.objects.all()
         return context
 
 class BookListView(ListView):
     model = Book
     template_name = 'book/book_list.html'
     context_object_name = 'books'
+
     def get_queryset(self):
-        queryset = Book.objects.all().order_by('title')  
+        queryset = Book.objects.all().order_by('title')
         category = self.kwargs.get('category')
         if category:
             queryset = queryset.filter(category__name=category)
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 def search_books(request):
     query = request.GET.get('q', ' ')
@@ -80,3 +83,34 @@ def book_list_by_category(request, category):
 
 def get_categories(request):
     return {'categories': Category.objects.all()}
+
+@login_required
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')  # run back to home 
+    else:
+        form = BookForm()
+    return render(request, 'book/add_book.html', {'form': form})
+
+@login_required
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('/')  # run back to home 
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'book/edit_book.html', {'form': form})
+
+@login_required
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('/')  # run back to home 
+    return render(request, 'book/delete_book.html', {'book': book})
